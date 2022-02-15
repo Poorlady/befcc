8a80198b9901d6ab36fc98094adc864fe3b2368b
Alif Sani Ekasmara <saniekasmara@gmail.com>
2022-02-15 15:41:27 +0700

    serving static asset

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var path = require("path");
 var app = express();
+const absolutePath = path.join(__dirname, "public");
+// console.log(absolutePath);
+app.use("/public", express.static(absolutePath));
 app.get('/', (req, res) => {
     // res.send("Hello Express");
-    const absolutePath = __dirname + "/views/index.html";
+    const absolutePath = path.join(__dirname, "/views/index.html");
     res.sendFile(absolutePath);
 });
 
