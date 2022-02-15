986ce78b0af54fc7bc2ac21f01a2eea0b1c375a5
Alif Sani Ekasmara <saniekasmara@gmail.com>
2022-02-15 15:25:33 +0700

    send file html

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 app.get('/', (req, res) => {
-    res.send("Hello Express");
+    // res.send("Hello Express");
+    const absolutePath = __dirname + "/views/index.html";
+    res.sendFile(absolutePath);
 });
 
 
