85f6b7bb5f1075e362a128f35c7d9e47ef6a1f1d
Alif Sani Ekasmara <saniekasmara@gmail.com>
2022-02-27 17:02:56 +0700

    add json route

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ var app = express();
 const absolutePath = path.join(__dirname, "public");
 // console.log(absolutePath);
 app.use("/public", express.static(absolutePath));
+app.get("/json", (req, res) => res.json({ message: "Hello json" }));
 app.get('/', (req, res) => {
     // res.send("Hello Express");
     const absolutePath = path.join(__dirname, "/views/index.html");
