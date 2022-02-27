6800c78715893e84e9bc996d547ecd65875f68b8
Alif Sani Ekasmara <saniekasmara@gmail.com>
2022-02-27 17:10:17 +0700

    add env file to project

--- myApp.js
+++ myApp.js
@@ -1,10 +1,19 @@
 var express = require('express');
+require("dotenv").config();
 var path = require("path");
 var app = express();
 const absolutePath = path.join(__dirname, "public");
 // console.log(absolutePath);
 app.use("/public", express.static(absolutePath));
-app.get("/json", (req, res) => res.json({ message: "Hello json" }));
+// route to serve json
+app.get("/json", (req, res) => {
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase();
+    }
+    res.json({ message: message });
+});
+// route to landing
 app.get('/', (req, res) => {
     // res.send("Hello Express");
     const absolutePath = path.join(__dirname, "/views/index.html");
