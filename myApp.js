6204cb70b1488464a9afc2b29013c721bf21f919
Alif Sani Ekasmara <saniekasmara@gmail.com>
2022-03-17 18:54:26 +0700

    fix removeManyPeople implementation use Model.remove()

--- myApp.js
+++ myApp.js
@@ -88,7 +88,7 @@ const removeById = async (personId, done) => {
 const removeManyPeople = async (done) => {
   const nameToRemove = "Mary";
 
-  const results = await Person.deleteMany({ name: nameToRemove });
+  const results = await Person.remove({ name: nameToRemove });
 
   done(null, results/*, data*/);
 };
