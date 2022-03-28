bce169a11d17db85385238ad098876083d4a8f25
Alif Sani Ekasmara <saniekasmara@gmail.com>
2022-03-28 15:20:01 +0700

    remove many people

--- myApp.js
+++ myApp.js
@@ -85,13 +85,13 @@ const removeById = async (personId, done) => {
   done(null, personDeleted /*, data*/);
 };
 
-const removeManyPeople = async (done) => {
+const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
   Person.remove({ name: nameToRemove }, (err, response) => {
     if (err) return console.log(err);
     done(null, response);
   });
-  // done(null, response/*, data*/);
+  // done(null/*, data*/);
 };
 
 // removeManyPeople((_, results) => console.log(results));
